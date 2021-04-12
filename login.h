894ee4fb6c723b2b73e40829d7d08e12bd69d294
Nguyen Nham Tan <nntan20@apcs.vn>
2021-04-12 11:52:01 +0700

    file dau tien

---
+++ login.h
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef login_h
+#define login_h
+
+struct User{
+     string name;
+     string pass;
+     bool isStaff = false;
+     bool isStudent = false;
+};
+
+#endif
+
+
+
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include"login.h"
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+int main()
+{
+
+}
