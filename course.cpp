86e8a24e312980728cd0911f84604b58e6dbcb50
MinhRose3003 <lnqminh20@apcs.vn>
2021-04-13 12:12:12 +0700

    sua 1 ti

--- course.cpp
+++ course.cpp
@@ -8,19 +8,17 @@ using namespace std;
 #include"student.h"
 
 
-void viewCourse(semester se[])
+void viewCourse(semester se[],int k)
 {
-    for (int k=0;k<3;k++)
-    {
-        cout<< "List Course in semester "<<k+1<<" : "<<endl;
+        cout<< "List Course in semester "<<k<<" : "<<endl;
         for(int i=0;i<7;i++)
         {
             cout <<" Course "<<i+1<< " : "<<endl;
-            cout <<"ID : "<<se[k].cs[i].id<<endl;
-            cout <<"Teacher name : " <<se[k].cs[i].teacherName<<endl;
-            cout <<"Number of credits : "<<se[k].cs[i].numOfCredits<<endl;
-            cout <<"Maximum of students : <<"se[k].cs[i].maxSt<<endl;
-            cout <<"Time in week : "<<se[k].cs[i].daySt[0].dayInWeek <<" - "<<se[k].cs[i].daySt[0].time <<" and " <<se[k].cs[i].daySt[1].dayInWeek<<" - " <<se[k].cs[i].daySt[1].time<<endl;
+            cout <<"ID : "<<se[k-1].cs[i].id<<endl;
+            cout <<"Teacher name : " <<se[k-1].cs[i].teacherName<<endl;
+            cout <<"Number of credits : "<<se[k-1].cs[i].numOfCredits<<endl;
+            cout <<"Maximum of students : "<<se[k-1].cs[i].maxSt<<endl;
+            cout <<"Time in week : "<<se[k-1].cs[i].daySt[0].dayInWeek <<" - "<<se[k-1].cs[i].daySt[0].time <<" and " <<se[k-1].cs[i].daySt[1].dayInWeek<<" - " <<se[k-1].cs[i].daySt[1].time<<endl;
+            cout <<endl;
         }
-    }
 }
