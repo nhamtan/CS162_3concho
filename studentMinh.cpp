1661218aa67a6390958761c82b4b242ef723c5e4
MinhRose3003 <lnqminh20@apcs.vn>
2021-04-15 20:42:01 +0700

    alo

---
+++ studentMinh.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#include"course.h"
+#include"date.h"
+#include "login.h"
+#include"staff.h"
+#include "student.h"
+#include <fstream>
+using namespace std;
+
+void studentLogInToSystem(ifstream &fin,User &us)
+{
+    fin.open("student.txt");
+    if(fin.is_open())
+    {
+        while()
+        {
+
+            string tmp;
+            getline(fin,tmp);
+            if(tmp == us.name)
+            {
+                getline(fin,tmp);
+                if(tmp == us.pass)
+                {
+                    us.isStaff = true;
+                    return;
+                }
+            }
+        }
+    }
+    fin.close();
+}
+
+
